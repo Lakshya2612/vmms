165ca513aa85398df1e6378add997aa0b1d3944f
Lakshya2612 <117802438+Lakshya2612@users.noreply.github.com>
2024-12-14 12:01:42 +0530

    Update app.js

--- mybackend/src/app.js
+++ mybackend/src/app.js
@@ -4,7 +4,10 @@ import cookieParser from "cookie-parser";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin:'https://vmms-kj74.onrender.com',
+  credentials: true
+}));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
