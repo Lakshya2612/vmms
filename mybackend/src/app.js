87158a88d94d09a96f501c07d3c68480e2747060
Lakshya <lakshyasharma262001@gmail.com>
2024-12-14 16:36:02 +0530

    environment

--- mybackend/src/app.js
+++ mybackend/src/app.js
@@ -5,7 +5,7 @@ import cookieParser from "cookie-parser";
 const app = express();
 
 app.use(cors({
-  origin:'https://vmms-kj74.onrender.com',
+  origin: process.env.URL,
   credentials: true
 }));
 
