fc37d79d98b37ee531902d79469e3f262d5c19db
Lakshya <lakshyasharma262001@gmail.com>
2024-12-16 11:36:50 +0530

    testing

--- mybackend/src/app.js
+++ mybackend/src/app.js
@@ -11,7 +11,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static("./public/temp"));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
