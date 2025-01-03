e9058b5d9916adbe6306b76f0c2a81af1d3f1c56
Lakshya <lakshyasharma262001@gmail.com>
2025-01-03 17:02:27 +0530

    trying vercel fix

--- mybackend/src/app.js
+++ mybackend/src/app.js
@@ -1,17 +1,20 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const app = express();
 
-app.use(cors({
-  origin: process.env.URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./public/temp"));
+app.use(express.static(path.join(__dirname, "./public/temp")));
 app.use(cookieParser());
 
 import userRouter from "./routes/user.routes.js";
